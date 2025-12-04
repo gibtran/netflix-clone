c46ac0aaf0705d0a60b2faf0e32e92e3fc5648cd
Brian Tran <giabaothichcho@gmail.com>
2025-12-04 23:08:52 +1100

    Final version

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
-
+import { ToastContainer } from 'react-toastify';
 const App = () => {
   const navigate = useNavigate()
   useEffect(() => {
@@ -21,6 +21,7 @@ const App = () => {
   }, [])
   return (
     <div>
+      <ToastContainer theme='dark'/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
